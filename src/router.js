8da05f7e51073e9a19268bf2b509cf1fffe01ad7
Catalin4Florin <catroa01@easv365.dk>
2025-10-21 17:57:48 +0200

    feat: add navigation guard to protect MyWorkouts and Profile routes for authenticated users only

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import HomePage from './components/HomePage.vue'
 import LoginPage from './components/LoginPage.vue'
 import MyWorkouts from './components/MyWorkouts.vue'
 import ProfilePage from './components/ProfilePage.vue'
+import { getAuth } from 'firebase/auth'
 
 const routes = [
   { path: '/', component: HomePage },
@@ -14,6 +15,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = getAuth()
+  const user = auth.currentUser
+
+  const requiresAuth = ['/myworkouts', '/profile'].includes(to.path)
+
+  if (requiresAuth && !user) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
